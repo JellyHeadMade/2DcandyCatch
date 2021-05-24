cb6dbcbfa762b449e9838919cd604697f9538b6a
JellyHead <JellyHeadM@outlook.com>
2021-05-24 19:48:06 -0400

    adding this older project to github as it was never pushed

---
+++ Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+    [SerializeField]
+    float maxX;
+
+    public GameObject[] Candies;
+
+    [SerializeField]
+    float SpawnInterval;
+
+    public static CandySpawner instance;
+
+    public bool GameOver { get; private set; }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SpawnCandy();
+
+        StartCoroutine("SpawnCandies");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnCandy()
+    {
+        //this what gives you a random number to spawn from
+        int rand = Random.Range(0, Candies.Length);
+
+        //this randomizes the postion using the maxX
+        float randX = Random.Range(-maxX, maxX);
+
+        //this creates a new vector3 using that random postion
+        Vector3 randomPos = new Vector3(randX, transform.position.y, transform.position.z);
+
+        //this is what acutally spawns them in place of the spawn object
+        Instantiate(Candies[rand], randomPos, transform.rotation);
+    }
+
+    IEnumerator SpawnCandies()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            SpawnCandy();
+
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+
+    public void startSpawingCandies()
+    {
+        StartCoroutine("SpawnCandies");
+    }
+
+    public void StopSpawningCandies()
+    {
+        StopCoroutine("SpawnCandies");
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject gameOverPanel;
+
+    int Lives = 3;
+
+    public static GameManager instance;
+
+    int Score = 0;
+
+    bool gameOver = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public Text livesCount;
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IncrementScore()
+    {
+        if (!gameOver)
+        {
+            Score++;
+            scoreText.text = Score.ToString();
+            print(Score);
+        }
+        
+    }
+
+    public void DecreaseLife()
+    {
+
+        
+
+        if (Lives > 0)
+        {
+            Lives--;
+            livesCount.text = Lives.ToString();
+            //print(Lives);
+        }
+
+        if(Lives <= 0)
+        {
+            gameOver = true;
+
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        CandySpawner.instance.StopSpawningCandies();
+
+        GameObject.Find("Player").GetComponent<PlayerController>().canMove = false;
+
+        print("GameOver");
+
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    
+}
